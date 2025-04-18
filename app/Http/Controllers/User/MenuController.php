f18390576e313ac99bce2d2bd49f5ee01cfd3da0
rezrfm <rez.rfm@gmail.com>
2025-04-18 21:00:32 +0700

    add public api to get menu

--- app/Http/Controllers/User/MenuController.php
+++ app/Http/Controllers/User/MenuController.php
@@ -6,8 +6,26 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\MerchantMenu;
+use Exception;
 
 class MenuController extends Controller
 {
-    
+    public function getAllMenus() {
+        try {
+            $menus = MerchantMenu::all();
+
+            return response()->json([
+                'message' => 'success',
+                'status' => 200,
+                'data' => $menus
+            ], 200);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'error',
+                'status' => 500,
+                'data' => null
+            ], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Cms\MerchantController;
 use App\Http\Controllers\Cms\MenuController as MerchantMenuController;
 use App\Http\Controllers\Cms\FoodController;
+use App\Http\Controllers\User\MenuController as PublicMenuController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -30,6 +31,8 @@ Route::middleware(['jwt.auth'])->group(function () {
 Route::get('/', [AuthController::class, 'test']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/menu', [PublicMenuController::class, 'getAllMenus']);
 
 Route::prefix('cms')->group(function () {
     Route::prefix('merchant')->group(function () {
