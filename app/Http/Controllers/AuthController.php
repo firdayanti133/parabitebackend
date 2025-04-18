64539b10fb7e315b2a5bd9edf4a587fe41eb4991
Firdayanti <yantifirda133@gmail.com>
2025-04-18 16:59:11 +0700

    login register add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(Request $request) {
-        $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-        ]); 
+    public function test() {
+        return response()->json(['success' => true]);
+    }
 
-        $credentials = $request->only('email', 'password');
+    public function register(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'message' => 'Validation Error',
+            'status' => 422,
+            'errors' => $validator->errors()
+        ], 422);
+    }
+
+    $customClaims = [
+        'exp' => now()->addDays(3)->timestamp,
+    ];
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    $token = JWTAuth::claims($customClaims)->fromUser($user);
+
+    return response()->json([
+        'message' => 'User successfully registered',
+        'access_token' => $token,
+        'token_type' => 'bearer',
+        'user' => $user
+    ], 201);
+}
+
+    
+    public function login(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'status' => 422,
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+         $credentials = $request->only('email', 'password');
 
         try {
             if (!$token = JWTAuth::attempt($credentials)) {
