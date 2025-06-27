2db5bfc1228aa469cd7c2694bab7df09f5d1e359
rezrfm <rez.rfm@gmail.com>
2025-06-28 00:43:01 +0700

    add cors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\CorsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'auth' => \App\Http\Middleware\AuthMiddleware::class,
         ]);
+        $middleware->prependToGroup('api', CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
