ed06cb7ff5b36d177a83b8657e0ff7c8ecba983e
rezrfm <rez.rfm@gmail.com>
2025-06-29 16:34:14 +0700

    fix temp order

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -64,12 +64,12 @@ class OrderController extends Controller
 
         try {
             $user = $request->get('auth_user');
-            $menuData = MenuRepository::getMenuMerchant($menuId);
-            $totalPrice = $menuData['price'] * $quantity;
+            $menuData = MenuRepository::getMenuDetail($menuId);
+            $totalPrice = $menuData['menu_price'] * $quantity;
 
             $data = [
                 'user_id' => $user->id,
-                'merchant_id' => $menuData,
+                'merchant_id' => $menuData['merchant_id'],
                 'menu_id' => $menuId,
                 'price' => $totalPrice,
                 'quantity' => $quantity,
